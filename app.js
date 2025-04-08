59b0b973982c1426f29ecff4d0e1afe1c1f70094
JesterAV <winchesrers@gmail.com>
2025-04-08 18:50:17 +0300

    save changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const dayOfWeekEN = [];
 const monthListRU = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
     'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
 ];
+const monthListEN = [];
 
 const greetings = document.getElementById('greetings');
 
@@ -25,7 +26,7 @@ function getRealTime() {
 
     dateAndDay.innerText = `${dayOfWeekRU[dayIndex]}, ${monthListRU[monthIndex]} ${date}`;
 
-    if (hour >= 22 && hour < 6) {
+    if (hour >= 22 || hour < 6) {
         greetings.innerText = 'Good night';
     } else if (hour >= 6 && hour < 12) {
         greetings.innerText = 'Good morning';
@@ -42,26 +43,55 @@ getRealTime();
 
 /// weather widget
 
+const cityName = document.getElementById('cityName');
+
 const weatherIcon = document.getElementById('weatherIcon');
 const temperature = document.getElementById('temperature');
 const windSpeed = document.getElementById('windSpeed');
 const humidity = document.getElementById('humidity');
+const weatherDiscription = document.getElementById('weatherDiscriptionInfo');
 
-async function getWeather() {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&lang=ru&appid=8f8888b555d073695608699843286db8&units=metric';
+async function getWeather(city) {
+    const apiKey = '8f8888b555d073695608699843286db8';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&lang=ru&appid=${apiKey}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
+    
+    const oldClass = Array.from(weatherIcon.classList).find(cls => cls.startsWith('owf-'));
+    if (oldClass) {
+        weatherIcon.classList.remove(oldClass);
+    }
 
-    // weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp | 0}°C`
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    cityName.value = data.name; 
+    temperature.textContent = `${data.main.temp | 0}°C`;
+    weatherDiscription.textContent = `${data.weather[0].main}`;
+    windSpeed.textContent = `Wind speed: ${data.wind.speed} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
     console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
 }
 
-getWeather();
+getWeather('Минск');
 
 /// Input Values
 
 const greetingsInput = document.getElementById('greetingsInput');
+
+let timeout;
+
+cityName.addEventListener('input', () => {
+
+    timeout = setTimeout(() => {
+        completeInput();
+    }, 2000);
+
+    function completeInput() {
+        const cityInput = cityName.value;
+        localStorage.setItem('city', cityInput);
+
+        getWeather(cityInput);
+    }
+});
 
 greetingsInput.addEventListener('input', () => {
     const inputValue = greetingsInput.value;
@@ -69,6 +99,171 @@ greetingsInput.addEventListener('input', () => {
     localStorage.setItem('greetingsInput', inputValue);
 });
 
+/// background image
+
+const root = document.getElementById('root');
+
+const imagesList = [];
+let currentImageIndex = -1;
+
+async function getImage() {
+    const accesKey = '-bCYGYOFTNih-GnchL2Vh78aFkemAX0ZFa4JE6MTCM4';
+    const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${accesKey}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    root.classList.add('hidden');
+
+    setTimeout(() => {
+        root.style.backgroundImage = `url(${data.urls.regular})`;
+        root.classList.remove('hidden');
+    }, 500);
+
+    imagesList.push(data.urls.regular);
+    currentImageIndex++;
+}
+
+getImage();
+
+const rightSliderButton = document.getElementById('rightSliderButton');
+const leftSliderButton = document.getElementById('leftSliderButton');
+
+
+
+leftSliderButton.addEventListener('click', () => {
+    if (imagesList.length === 0 || currentImageIndex <= 0) {
+        return;
+    }
+
+    currentImageIndex--;
+    
+    root.classList.add('hidden');
+
+    setTimeout(() => {
+        root.style.backgroundImage = `url(${imagesList[currentImageIndex]})`;
+        root.classList.remove('hidden');
+    }, 500);
+});
+
+rightSliderButton.addEventListener('click', getImage);
+
+/// Quote
+
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+async function getQuote() {
+    const url = 'https://api.api-ninjas.com/v1/quotes';
+    const apiKey = 'Lq08Jds9xI+iQDfO4EBD9w==HKa2xMNgCTem72pP';
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        quote.textContent = `${data[0].quote}`
+        author.textContent = `${data[0].author}`
+        console.log(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+getQuote();
+
+const changeQuoteButton = document.getElementById('changeQuote');
+
+changeQuoteButton.addEventListener('click', getQuote);
+
+/// Player
+
+const volume = document.getElementById('volume');
+
+const playButton = document.getElementById('playButton');
+const playNextButton = document.getElementById('play-next');
+const prevButton = document.getElementById('play-prev');
+
+const firstSong = document.getElementById('first-song');
+const secondSong = document.getElementById('second-song');
+
+const songs = [firstSong, secondSong];
+
+let isPlaying = false;
+let counter = 0;
+
+function playSongs() {
+
+    document.querySelectorAll('.song').forEach(song => {
+        song.classList.remove('active-song');
+    });
+
+    if (isPlaying) {
+        songs[counter].pause();
+        playButton.classList.remove('active');
+    } else {
+        songs[counter].play();
+        playButton.classList.add('active');
+
+        document.querySelectorAll('.song')[counter].classList.add('active-song');
+    }
+
+    isPlaying = !isPlaying;
+}
+
+prevButton.addEventListener('click', () => {
+    if (counter === 0) {
+        return;
+    }
+
+    songs[counter].pause();
+    songs[counter].currentTime = 0;
+    isPlaying = false;
+    
+    counter--;
+    playSongs();
+});
+
+playNextButton.addEventListener('click', () => {
+    songs[counter].pause();
+
+    if (counter === songs.length - 1) {
+        counter = 0;
+    } else {
+        counter++;
+    }
+
+    songs[counter].currentTime = 0;
+    isPlaying = false;
+
+    playSongs();
+});
+
+playButton.addEventListener('click', playSongs);
+
+volume.addEventListener('input', () => {
+    const realValue = volume.value / 100;
+    songs[counter].volume = realValue;
+
+    localStorage.setItem('savedVolume', realValue);
+});
+
 window.addEventListener('load', () => {
     greetingsInput.value = localStorage.getItem('greetingsInput');
+
+    const savedVolume = localStorage.getItem('savedVolume');
+
+    if (savedVolume != null) {
+        volume.value = savedVolume * 100;
+        songs[counter].volume = savedVolume;
+    }
 });
